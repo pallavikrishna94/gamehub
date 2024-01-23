76947b2fd5bb32f2f08e70266440486a6215359a
Pallavi <krishnapallavi.994@gmail.com>
2024-01-23 18:37:04 -0500

    creating a generic datra fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-
 export interface Games {
   id: number;
   name: string;
@@ -16,35 +13,6 @@ export interface Games {
   metacritic: number;
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Games[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Games[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false)
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Games> ("/games")
 
 export default useGames;
