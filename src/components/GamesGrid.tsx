f35e80b9ed05b709fa7cc26a5d3a3f3cb6f23b53
Pallavi <krishnapallavi.994@gmail.com>
2024-01-23 19:48:53 -0500

    display genres

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -13,7 +13,7 @@ const GamesGrid = () => {
       <SimpleGrid
         column={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,9 @@
 import useData from "./useData";
 
 export interface Genres {
-    id: number,
-    name: string
+    id: number;
+    name: string;
+    image_background: string;
 }
 
 const useGenres = () => useData<Genres> ("/genres");
